c167a97dbfef3e3e6e37057467cf02ad627e2f2d
nebbilies <hirohamada17092005@gmail.com>
2025-12-02 16:14:15 +0700

    feat: add breadcrumb component

---
+++ src/app/(storefront)/products/[id]/page.tsx
@@ -0,0 +1,34 @@
+import { ProductDetailBreadcrumb } from "@/app/(storefront)/products/[id]/_components/ProductDetailBreadcrumb/ProductDetailBreadcrumb";
+import { Product, ProductResponse } from "@/types/types";
+import { ProductDetailSection } from "@/app/(storefront)/products/[id]/_components/ProductDetailSection/ProductDetailSection";
+
+interface ProductDetailPageProps {
+  params: { id: string };
+}
+
+export default async function ProductDetailPage({
+  params,
+}: ProductDetailPageProps) {
+  const { id } = await params;
+  const data = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/products?product_ids=${id}`,
+  );
+  if (!data.ok) {
+    const msg = await data.json();
+    console.error("Error fetching product data:", msg.message);
+    throw new Error("Failed to fetch product data");
+  }
+  const productData: ProductResponse = await data.json();
+  const product: Product = productData.data[0];
+
+  return (
+    <main
+      className={
+        "mx-auto flex w-full max-w-7xl flex-col items-center justify-center gap-10 p-4"
+      }
+    >
+      <ProductDetailBreadcrumb product={product} />
+      <ProductDetailSection product={product} />
+    </main>
+  );
+}
