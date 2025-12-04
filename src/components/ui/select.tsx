81e966769b761c5f9c2c1199e0ab93bcef09527d
nebbilies <hirohamada17092005@gmail.com>
2025-12-05 00:32:52 +0700

    chore: add shadcn ui components

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -21,7 +21,7 @@ function SelectGroup({
 function SelectValue({
   ...props
 }: React.ComponentProps<typeof SelectPrimitive.Value>) {
-  return <SelectPrimitive.Value data-slot="select-value" {...props} />;
+  return <SelectPrimitive.Value data-slot="select-values" {...props} />;
 }
 
 function SelectTrigger({
