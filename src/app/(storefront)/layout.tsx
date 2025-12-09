6bf4e4e227c6f4fdc88de5481cb6147dc30f98b6
nebbilies <hirohamada17092005@gmail.com>
2025-12-09 16:13:03 +0700

    chore: add Toast to storefront layout

--- src/app/(storefront)/layout.tsx
+++ src/app/(storefront)/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/app/(storefront)/_components/Header/Header";
 import Footer from "@/app/(storefront)/_components/Footer/Footer";
 import { OrderProvider } from "@/app/context";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function StorefrontLayout({
   children,
@@ -11,6 +12,7 @@ export default function StorefrontLayout({
     <OrderProvider>
       <div className="mx-auto flex min-h-screen w-full flex-col bg-white">
         <div className="flex-grow">
+          <Toaster position="bottom-right" richColors theme={"light"} />
           <Header />
           <div className={"flex flex-col items-center"}>{children}</div>
         </div>
