208a106bbb6c3d0d7129817ba99bbc65e2818ed0
nebbilies <hirohamada17092005@gmail.com>
2025-12-10 14:57:43 +0700

    feat: update rabc for /admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,22 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+import { UserRole } from "@/types/roles";
+
+export default auth((req) => {
+  const { pathname } = req.nextUrl;
+
+  if (!req.auth?.user) {
+    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+  }
+
+  if (pathname.startsWith("/admin")) {
+    if (req.auth.role !== UserRole.ADMIN && req.auth.role !== UserRole.STAFF) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
