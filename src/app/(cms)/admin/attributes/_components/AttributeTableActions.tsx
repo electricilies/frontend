e0fe7020b9e5b85c29321f184dc86a89efae3154
nebbilies <hirohamada17092005@gmail.com>
2025-12-10 14:58:36 +0700

    fix: delete attribute not working correctly

--- src/app/(cms)/admin/attributes/_components/AttributeTableActions.tsx
+++ src/app/(cms)/admin/attributes/_components/AttributeTableActions.tsx
@@ -4,6 +4,7 @@ import { EditButton } from "@/app/(cms)/_components/EditButton/EditButton";
 import { AttributeDialogContent } from "@/app/(cms)/admin/attributes/_components/AttributeDialog";
 import { Attribute } from "@/types/types";
 import { DeleteButton } from "@/app/(cms)/_components/DeleteButton/DeleteButton";
+import { useSession } from "next-auth/react";
 
 interface AttributeTableActionsProps {
   attribute: Attribute;
@@ -11,11 +12,17 @@ interface AttributeTableActionsProps {
 export default function AttributeTableActions({
   attribute,
 }: AttributeTableActionsProps) {
-  const handleDelete = async () => {
+  const session = useSession();
+  const token = session.data?.accessToken;
+
+  const handleDelete = async (attributeId: string) => {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/attributes/{id}`,
+      `${process.env.NEXT_PUBLIC_BASE_URL}/attributes/${attributeId}`,
       {
         method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       },
     );
     if (!response.ok) {
@@ -35,7 +42,7 @@ export default function AttributeTableActions({
           />
         )}
       </EditButton>
-      <DeleteButton onDelete={() => handleDelete()} />
+      <DeleteButton onDelete={() => handleDelete(attribute.id)} />
     </>
   );
 }
